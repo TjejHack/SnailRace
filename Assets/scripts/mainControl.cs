e41edefa9fd1cb138bc17d209110b4a1be19fe94
AdrinaW <danw@adrina.se>
2014-11-10 18:34:25 +0100

    Upload
    Upload of files

---
+++ Assets/scripts/mainControl.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+
+public class mainControl : MonoBehaviour {
+
+	public float strafeSpeed = 1.0F;
+	public float Speed = 100.0F;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKey ("up")) {
+			transform.Translate((Vector3.forward * (Time.deltaTime)) * Speed);
+		}
+
+		// Setting up left and right control
+		if (Input.GetKey("left")) {
+			//Debug.Log(Input.GetKey("left"));
+			transform.Translate(Vector3.left * (Time.deltaTime * strafeSpeed) );
+		}
+		else if(Input.GetKey ("right")) {
+			transform.Translate(Vector3.right * (Time.deltaTime * strafeSpeed) );
+		}
+
+
+		/*if (Input.GetButtonDown("Horizontal")) {
+			Debug.Log(Input.GetKey("left"));
+		}*/
+	}
+
+	void Example() {
+		collider.material.dynamicFriction = 1;
+	}
+}
